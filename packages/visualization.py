edd48c510c20f0008a2087fe81a77dac45133ea3
CHANGGYU SONG <song4893@gmail.com>
2018-06-05 14:08:43 -0700

    Modifying AnimatedPlot Add description

--- packages/visualization.py
+++ packages/visualization.py
@@ -1,3 +1,5 @@
+"""Summary
+"""
 import numpy as np
 import matplotlib as matplot
 import matplotlib.pyplot as plt
@@ -10,12 +12,34 @@ import matplotlib.animation as animation
 import datetime
 
 class StaticPlot:
+
+    """Plot Class to build a static plot
+    
+    Attributes:
+        ax (plt.Axes): Subplot
+        ax_im (plt.axesImage): Image plot in "ax"
+        date (str): Date
+        fig (plt.figure): Figure of plots
+        slider (ipywidget.Slider): Slider widget
+        tileCol (int): Column of tile
+        tileMatrix (int): Zoom in level
+        tileRow (int): Row of tile
+    """
+    
     def __init__(
             self,
             tileMatrix=5,
             tileCol=6,
             tileRow=5,
             date="2017-10-31"):
+        """Implementing information of tiles
+        
+        Args:
+            tileMatrix (int, optional): Zoom in level
+            tileCol (int, optional): Column
+            tileRow (int, optional): Row
+            date (str, optional): Date
+        """
         self.fig = None
         self.ax = None
         self.ax_im = None
@@ -28,6 +52,8 @@ class StaticPlot:
         self.slider = None
 
     def subplot(self):
+        """Integrating functions and plotting.
+        """
         self.fig, self.ax = plt.subplots()
         self.create_imshow()
         self.create_buttons()
@@ -35,6 +61,8 @@ class StaticPlot:
         self.render()
 
     def create_imshow(self):
+        """Create image plot and set extent
+        """
         extent = (
             self.top_left[1],
             self.bot_right[1],
@@ -45,6 +73,8 @@ class StaticPlot:
         self.ax_im = self.ax.imshow(np.zeros((512, 512)), extent=extent)
 
     def create_buttons(self):
+        """Create buttons and set functions
+        """
         # Create buttons
         button_left  = Button(description='Left')
         button_right = Button(description='Right')
@@ -64,6 +94,8 @@ class StaticPlot:
         display(all_buttons)
 
     def create_slider(self):
+        """Create slider and set function
+        """
         # Create a slider
         self.slider = IntSlider(
             value=5,
@@ -82,6 +114,8 @@ class StaticPlot:
         display(self.slider)
 
     def render(self):
+        """Rendering(Changing) image on the plot
+        """
         img_array = getimage.get_image(
             tileMatrix = self.tileMatrix,
             tileCol = self.tileCol,
@@ -97,6 +131,12 @@ class StaticPlot:
 
     @property
     def top_left(self):
+        """Coordinates of top left
+        
+        Returns:
+            latitude: latitude of top left corner
+            longitude: longitude of top left corner
+        """
         return conversion.get_coordinates(
             tileMatrix = self.tileMatrix,
             tileCol = self.tileCol,
@@ -105,6 +145,12 @@ class StaticPlot:
 
     @property
     def bot_right(self):
+        """Coordinates of bottom right
+        
+        Returns:
+            latitude: latitude of bottom right corner
+            longitude: longitude of bottom right corner
+        """
         return conversion.get_coordinates(
             tileMatrix = self.tileMatrix,
             tileCol = self.tileCol + 1,
@@ -112,6 +158,8 @@ class StaticPlot:
         )
 
     def update_ticks(self):
+        """Update coordinates corresponds to the tile
+        """
         yticks = np.linspace(self.top_left[0], self.bot_right[0], 10)
         xticks = np.arange(self.top_left[1], self.bot_right[1],
             (self.bot_right[1] - self.top_left[1]) / 4.5)
@@ -120,22 +168,47 @@ class StaticPlot:
         self.ax.set_xticklabels(xticks)
 
     def left_cb(self, event=None):
+        """Function for left button widget
+        
+        Args:
+            event (Event, optional): Button click
+        """
         self.tileCol -= 1
         self.render()
 
     def right_cb(self, event=None):
+        """Function for right button widget
+        
+        Args:
+            event (None, optional): Button click
+        """
         self.tileCol += 1
         self.render()
 
     def up_cb(self, event=None):
+        """Function for up button widget
+        
+        Args:
+            event (None, optional): Button click
+        """
         self.tileRow -= 1
         self.render()
 
     def down_cb(self, event=None):
+        """Function for down button widget
+        
+        Args:
+            event (None, optional): Button click
+        """
         self.tileRow += 1
         self.render()
 
     def slider_cb(self, event):
+        """Function for slider widget
+        
+        Args:
+            event (TYPE): changed slider point
+        """
         center_latitude = (self.top_left[0] + self.bot_right[0]) / 2.0
         center_longitude = (self.top_left[1] + self.bot_right[1]) / 2.0
 
@@ -149,6 +222,22 @@ class StaticPlot:
         self.render()
 
 class AnimatedPlot:
+
+    """Plot Class to build animation
+    
+    Attributes:
+        ax (plt.Axes): Subplot
+        ax_im (plt.axesImage): Image plot in "self.ax"
+        dates (list): list of dates in str format
+        end_date (str): End date of Animation
+        fig (plt.figure): Figure of plots
+        img (list): List of Images from start date to end date
+        start_date (str): Start date of Animation
+        tileCol (int): Column
+        tileMatrix (int): Zoom in level
+        tileRow (int): Row
+    """
+    
     def __init__(
             self,
             tileMatrix=4,
@@ -156,18 +245,43 @@ class AnimatedPlot:
             tileRow=2,
             start_date = '2017-10-01', 
             end_date = '2017-10-10'):
-
+        """Initialize the starting tile
+        
+        Args:
+            tileMatrix (int, optional): Zoom in level
+            tileCol (int, optional): Column
+            tileRow (int, optional): Row
+            start_date (str, optional): Start date of Animation
+            end_date (str, optional): End date of Animation
+        """
         self.start_date = start_date
         self.end_date = end_date
+        self.dates = []
 
         self.fig = None
         self.ax = None
         self.ax_im = None
+        self.img = None
 
         self.tileMatrix = tileMatrix
         self.tileCol = tileCol
         self.tileRow =  tileRow
 
+    def subplot(self):
+        """Integrate and starting the animation
+        
+        Returns:
+            plt.Animation: Function that has Animation
+        """
+        self.fig, self.ax = plt.subplots()
+        self.load_image()
+        self.get_dates()
+        self.create_imshow()
+        return self.create_animate()
+
+    def load_image(self):
+        """Make all images to a list
+        """
         #Downloading Images
         img_array = getimage.get_image_date_range(
             tileMatrix = self.tileMatrix,
@@ -179,36 +293,36 @@ class AnimatedPlot:
         #Make an array of image
         self.img = [Image.fromarray(i) for i in img_array]
 
-    def subplot(self):
-        self.fig, self.ax = plt.subplots()
-        self.create_imshow()
-        ani = self.create_animate()
-        return ani
-
     def create_imshow(self):
+        """Create subplot
+        """
         extent = (
             self.top_left[1],
             self.bot_right[1],
             self.top_left[0],
             self.bot_right[0]
         )
+        #Put initialized Image data in self.ax_im
+        self.ax_im = plt.imshow(np.zeros((512, 512)), extent=extent)
 
-        self.ax_im = self.ax.imshow(np.zeros((512, 512)), extent=extent)
-
-    @property
-    def dates(self):
-        # Make an array of date string
-        dates = []
+    def get_dates(self):
+        """Get a list of date string, Implement in self.dates
+        """
         start_date = datetime.datetime.strptime(self.start_date,'%Y-%m-%d')
         end_date = datetime.datetime.strptime(self.end_date,'%Y-%m-%d')
         step = datetime.timedelta(days=1)
         while start_date<=end_date:
-            dates.append(str(start_date.date()))
+            self.dates.append(str(start_date.date()))
             start_date += step
-        return dates
 
     @property
     def bot_right(self):
+        """Coordinates of bottom right
+        
+        Returns:
+            latitude: latitude of bottom right corner
+            longitude: longitude of bottom right corner
+        """
         return conversion.get_coordinates(
             tileMatrix = self.tileMatrix,
             tileCol = self.tileCol + 1,
@@ -217,6 +331,12 @@ class AnimatedPlot:
 
     @property
     def top_left(self):
+        """Coordinates of top left
+        
+        Returns:
+            latitude: latitude of top left corner
+            longitude: longitude of top left corner
+        """
         return conversion.get_coordinates(
             tileMatrix = self.tileMatrix,
             tileCol = self.tileCol,
@@ -224,8 +344,22 @@ class AnimatedPlot:
         )
 
     def create_animate(self):
+        """Create animation
+
+        Returns:
+            plt.Animation: Function that has animation
+
+        """
         # create animate function
         def update_fig(frame):
+            """Function to change the image on the plot
+            
+            Args:
+                frame (int): initial number of image
+            
+            Returns:
+                axes.Image: Plot of changed Image
+            """
             #updating images on every frame
             self.ax_im.set_data(self.img[frame])
             self.ax.set_title(self.dates[frame])
